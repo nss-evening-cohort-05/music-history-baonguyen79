2babc8352f9f0b7611c6ced327178e4399a2ed96
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-04-08 09:39:30 -0500

    add

--- song.js
+++ song.js
@@ -5,7 +5,7 @@ songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs[songs.length] = "Ironi!c >by Alanis Moris*ette on the album Jagged Little Pill";
 
 songs.unshift('Stairway to Heaven > Maurizio Colonna album Rock Waves genre Rock');
 
